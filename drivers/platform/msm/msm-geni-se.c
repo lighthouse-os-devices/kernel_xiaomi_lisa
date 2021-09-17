e88edf872c8218460b17dd07c9bca9b82db863fb
Juhyung Park <qkrwngud825@gmail.com>
2021-09-17 22:08:48 +0900

    treewide: remove remaining _no_log() usage
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- drivers/platform/msm/msm-geni-se.c
+++ drivers/platform/msm/msm-geni-se.c
@@ -122,7 +122,7 @@ struct geni_se_device {
  */
 unsigned int geni_read_reg_nolog(void __iomem *base, int offset)
 {
-	return readl_relaxed_no_log(base + offset);
+	return readl_relaxed(base + offset);
 }
 EXPORT_SYMBOL(geni_read_reg_nolog);
 
@@ -134,7 +134,7 @@ EXPORT_SYMBOL(geni_read_reg_nolog);
  */
 void geni_write_reg_nolog(unsigned int value, void __iomem *base, int offset)
 {
-	return writel_relaxed_no_log(value, (base + offset));
+	return writel_relaxed(value, (base + offset));
 }
 EXPORT_SYMBOL(geni_write_reg_nolog);
 
